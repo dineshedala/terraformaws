91fd70deb56e128cddf5e5f939550142e2fbbed5
E.Dinesh <dineshe24@gmail.com>
2026-08-22 22:38:15 +0530

    Fix: Correct source_arn format in aws_lambda_permission resource
    - Changed source_arn from local.bucket_id (bucket name) to arn:aws:s3:::${local.bucket_id}
    - AWS Lambda permission requires a valid ARN format, not just the bucket name
    - Also fixed lambda_s3_access policy Resource to use full ARN format
    - Resolves error: "source_arn is an invalid ARN: arn: invalid prefix"

--- lambda.tf
+++ lambda.tf
@@ -53,7 +53,7 @@ resource "aws_iam_role_policy" "lambda_s3_access" {
         "s3:GetObject",
         "s3:PutObject"
       ]
-      Resource = "${local.bucket_id}/*"
+      Resource = "arn:aws:s3:::${local.bucket_id}/*"
     }]
   })
 
@@ -79,7 +79,7 @@ resource "aws_lambda_permission" "allow_s3" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.csv_processor.function_name
   principal     = "s3.amazonaws.com"
-  source_arn    = local.bucket_id
+  source_arn    = "arn:aws:s3:::${local.bucket_id}"
 }
 
 # Configure S3 bucket to trigger Lambda on CSV uploads
